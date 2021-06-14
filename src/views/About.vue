1255583befdcd2261f9d764656e32c0d3d56133d
Piersandro Guerrera <piersandro.guerrera@food-chain.it>
2021-06-14 11:41:17 +0200

    Added requrements

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,9 +5,11 @@
         <h3 class="title">Quadrans Staking</h3>
           <h4 class="title">What is Quadrans Token Staking Reward?</h4>
           <p><img src="https://quadrans.io/assets/img/illo_ethical.png" align="right" style="max-width: 250px; width: 100%;">Quadrans Tokens Staking refers to holding your cryptocurrency funds in a smart contract and receive a reward after a period ot time.</p>
-          <p>The QDT are being locked and for your activity you will be rewarded by the Quadrans Foundation in return.</p>
+          <p>The QDT will be locked and for your activity you will be rewarded by the Quadrans Foundation in return.</p>
           <p>Staking provides a way of making an income in QDT. For your partecipation in Quadrans Tokens Staking the Foundation recognize you an <u>yearly interest rate of 14%</u>. The minimum amount of QDT you can stake is <u>10000 Quadrans Token</u>.</p>
           <p>You can stake/unstake you tokens whenever you want but for every movement you need to pay a fee to the Ethereum network.</p>
+          <h4 class="title">Requirements</h4>
+          <p>Quadrans Token holders can stake their QDT directly from their <a href="https://metamask.io" target="_blank">MetaMask</a> application. You'll need some ETH (Ether) to pay the transactions required to confirm the use of this tool and Stake/Unstake your Tokens.</p>
           <h4 class="title">About Quadrans Token</h4>
           <ul>
             <li><p>‣ <b>Symbol:</b> QDT</p></li>
@@ -17,7 +19,7 @@
           <h4 class="title">What is Quadrans?</h4>
           <p><b>Quadrans</b> is a public blockchain infrastructure that runs Smart Contracts and decentralised applications (dApps), with particular focus on improving the major challenges that blockchain ecosystems are facing today such as security, interoperability, scalability and high operational costs.</p>
           <p>With a mind focused on the needs of Industry, complex supply chains, IOT devices, Quadrans finds its roots in a fork of the Go-Ethereum's source code. To keep the cost of operations balanced, the distribution of Quadrans Coin (QDC) required to write on the blockchain is carried out through a minting protocol.</p>
-          <h4><em>About Quadrans</em></h4>
+          <h4 class="title"><em>About Quadrans</em></h4>
             <ul>
               <li><p>‣ <a href="https://quadrans.io">Quadrans Foundation website</a></p></li>
               <li><p>‣ <a href="https://docs.quadrans.io">Quadrans blockchain Documentation</a></p></li>
