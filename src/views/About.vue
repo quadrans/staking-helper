c604d125edc25ff0c6b59f8f9d6f55e8d7f1d00c
Piersandro Guerrera <piersandro.guerrera@food-chain.it>
2022-09-02 16:35:12 +0200

    Fixed Staking Monitor link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,147 +1,196 @@
 <template>
-<div class="container-fluid px-0 pt-0">
-    
-      <div class="container-fluid bg-map-grey first-section row-before-blue">
-        <div class="row first-section">
-          <div class="col-12 col-lg-6">
-            <div class="row">
-              <div class="col-12 col-lg-8 mb-3 mb-lg-0">
-                <h1>What is <b>Quadrans Token Staking Reward? </b></h1>
-                <p>Quadrans Token Staking refers to holding your cryptocurrency funds in a smart contract and receive a reward after a period of time. 
-                   The QDT will be locked and you will be rewarded for your activity by the Quadrans Foundation.
-                </p>
-                <router-link to="/">
-                  <b-button
-                    type="fill"
-                    >Stake NOW</b-button
-                  > 
-                </router-link>             
-                <a href="https://monitor.quadrans.io/" target="_blank">
-                  <b-button
-                    type="fill"
-                    >Staking Monitor</b-button
-                  > 
-                </a>             
+  <div class="container-fluid px-0 pt-0">
+    <div class="container-fluid bg-map-grey first-section row-before-blue">
+      <div class="row first-section">
+        <div class="col-12 col-lg-6">
+          <div class="row">
+            <div class="col-12 col-lg-8 mb-3 mb-lg-0">
+              <h1>What is <b>Quadrans Token Staking Reward? </b></h1>
+              <p>
+                Quadrans Token Staking refers to holding your cryptocurrency
+                funds in a smart contract and receive a reward after a period of
+                time. The QDT will be locked and you will be rewarded for your
+                activity by the Quadrans Foundation.
+              </p>
+              <router-link to="/">
+                <b-button type="fill">Stake NOW</b-button>
+              </router-link>
+              <router-link to="/statistics">
+                <b-button type="fill">Staking Statistics</b-button>
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="row card-q d-flex bg-white p-4">
+            <div class="col-12 col-sm-2 text-center align-self-center">
+              <h1 class="violet count mb-0"><b>14%</b></h1>
+              <p class="mb-0 font-italic"><b>APY</b></p>
+            </div>
+            <div class="col-12 col-sm-10">
+              <p class="mb-0">
+                Staking provides a way of making an income in QDTs. For your
+                partecipation in Quadrans Tokens Staking the Foundation
+                recognize you an <b>yearly reward of 14%</b>.
+              </p>
+            </div>
+          </div>
+          <div class="row card-q d-flex bg-white p-4">
+            <div class="col-12 col-sm-2 text-center align-self-center">
+              <h1 class="violet count mb-0"><b>10k</b></h1>
+              <p class="mb-0 font-italic"><b>QDT</b></p>
+            </div>
+            <div class="col-12 col-sm-10">
+              <p class="mb-0">
+                The minimum amount of QDT you can stake is
+                <b>10.000 Quadrans Token</b>. You can stake/unstake your tokens
+                whenever you want but for every movement you need to pay a fee
+                to the Ethereum network.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-lg-6">
-            <div class="row card-q d-flex bg-white p-4">
-              <div class="col-12 col-sm-2 text-center align-self-center">
-                <h1 class="violet count mb-0"><b>14%</b></h1>
-                <p class="mb-0 font-italic"><b>APY</b></p>
-              </div>
-              <div class="col-12 col-sm-10">
-                <p class="mb-0">
-                  Staking provides a way of making an income in QDTs. For your partecipation in Quadrans Tokens Staking the Foundation recognize you an <b>yearly reward of 14%</b>.
+    <div class="container-fluid bg-blue-before p-0 top-negative-190">
+      <img class="img-fluid" src="/img/oblique_top.png" />
+    </div>
+    <div class="bg-blue container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <div class="row">
+            <div class="col-12 col-lg-6 my-auto pr-xl-5 text-left">
+              <h1 class="white">Staking <b>requirements</b></h1>
+              <div style="border-left: 8px solid #13fac8; padding-left: 15px;">
+                <h5 class="white font-weight-bold" style="text-transform:none;">
+                  Getting started is very easy!
+                </h5>
+                <p class="white mb-3 mb-lg-0">
+                  Quadrans Token holders can stake QDT directly from their
+                  <b>MetaMask</b> application. You’ll need some
+                  <b>ETH</b> (Ether) to pay the transactions required to confirm
+                  this tool usage and Stake/Unstake your Tokens.
                 </p>
               </div>
             </div>
-            <div class="row card-q d-flex bg-white p-4">
-              <div class="col-12 col-sm-2 text-center align-self-center">
-                <h1 class="violet count mb-0"><b>10k</b></h1>
-                <p class="mb-0 font-italic"><b>QDT</b></p>
-              </div>
-              <div class="col-12 col-sm-10">
-                <p class="mb-0">
-                  The minimum amount of QDT you can stake is <b>10.000 Quadrans Token</b>. You can stake/unstake your tokens whenever you want but for every movement you need to pay a fee to the Ethereum network.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-      </div>
-   
-      <div class="container-fluid bg-blue-before p-0 top-negative-190"><img class="img-fluid" src="/img/oblique_top.png"></div>
-      <div class="bg-blue container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <div class="row">
-              <div class="col-12 col-lg-6 my-auto pr-xl-5 text-left">
-                <h1 class="white">Staking <b>requirements</b></h1>
-                <div style="border-left: 8px solid #13fac8; padding-left: 15px;">
-                  <h5 class="white font-weight-bold" style="text-transform:none;">Getting started is very easy!</h5>
-                  <p class="white mb-3 mb-lg-0">
-                    Quadrans Token holders can stake QDT directly from their <b>MetaMask</b> application. You’ll need some <b>ETH</b> (Ether) 
-                    to pay the transactions required to confirm this tool usage and Stake/Unstake your Tokens.
-                  </p>
+            <div
+              class="col-12 col-lg-6 justify-content-center d-none d-md-flex"
+            >
+              <div class="row">
+                <div class="col-6 align-self-center">
+                  <img
+                    class="img-fluid"
+                    src="/img/eth.png"
+                    style="max-height: 100px;"
+                  />
                 </div>
-              </div>
-              <div class="col-12 col-lg-6 justify-content-center d-none d-md-flex">
-                <div class="row">
-                  <div class="col-6 align-self-center">
-                    <img class="img-fluid" src="/img/eth.png" style="max-height: 100px;">
-                  </div>
-                  <div class="col-6 align-self-center">
-                    <img class="img-fluid" src="/img/metamask.png" style="max-height: 150px;">
-                  </div>
+                <div class="col-6 align-self-center">
+                  <img
+                    class="img-fluid"
+                    src="/img/metamask.png"
+                    style="max-height: 150px;"
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="container-fluid bg-blue-after p-0"><img class="img-fluid" src="/img/oblique_bottom.png"></div>
-      
-    
-      <div class="container-fluid row-after-blue">
-        <div class="row">
-          <div class="col-6">
-            <h1>What is <b>Quadrans</b>?</h1>
-          </div>
+    </div>
+    <div class="container-fluid bg-blue-after p-0">
+      <img class="img-fluid" src="/img/oblique_bottom.png" />
+    </div>
+
+    <div class="container-fluid row-after-blue">
+      <div class="row">
+        <div class="col-6">
+          <h1>What is <b>Quadrans</b>?</h1>
         </div>
-      
-        <div class="row mt-4">
-          <div class="col-12 col-lg-6 mb-3 mb-lg-0">
-            <p>
-              Quadrans is a public blockchain infrastructure that runs Smart Contracts and decentralised applications (dApps), with particular focus on improving 
-              the major challenges that blockchain ecosystems are facing today such as security, interoperability, scalability and high operational costs.
-            </p>
-            <p>
-              Focusing on the Industry needs, complex supply chains, IoT devices, Quadrans finds its roots in a fork of the Go-Ethereum’s source code. To keep the cost of operations balanced, the distribution of Quadrans Coin (QDC) required to write on the blockchain is carried out through a minting protocol.
-            </p>
-          </div>
-          <div class="col-12 col-lg-6">
-            <div class="row">
-              <div class="col">
-                <h6 class="violet text-uppercase font-weight-bold">About Quadrans Token</h6>
-                <ul class="list-style mt-3">
-                  <li class="py-1">Symbol: <b>QDT</b>
-                  </li>
-                  <li class="py-2 text-break">
-                    Contract: 
-                    <a target="_blank" href="https://etherscan.io/address/0x9adc7710e9d1b29d8a78c04d52d32532297c2ef3">0x9adc7710e9d1b29d8a78c04d52d32532297c2ef3</a>
-                  </li>
-                  <li class="py-1">Max Total Supply: <b>600 Milions</b>
-                  </li>
-                </ul>
-              </div>
+      </div>
+
+      <div class="row mt-4">
+        <div class="col-12 col-lg-6 mb-3 mb-lg-0">
+          <p>
+            Quadrans is a public blockchain infrastructure that runs Smart
+            Contracts and decentralised applications (dApps), with particular
+            focus on improving the major challenges that blockchain ecosystems
+            are facing today such as security, interoperability, scalability and
+            high operational costs.
+          </p>
+          <p>
+            Focusing on the Industry needs, complex supply chains, IoT devices,
+            Quadrans finds its roots in a fork of the Go-Ethereum’s source code.
+            To keep the cost of operations balanced, the distribution of
+            Quadrans Coin (QDC) required to write on the blockchain is carried
+            out through a minting protocol.
+          </p>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="row">
+            <div class="col">
+              <h6 class="violet text-uppercase font-weight-bold">
+                About Quadrans Token
+              </h6>
+              <ul class="list-style mt-3">
+                <li class="py-1">Symbol: <b>QDT</b></li>
+                <li class="py-2 text-break">
+                  Contract:
+                  <a
+                    target="_blank"
+                    href="https://etherscan.io/address/0x9adc7710e9d1b29d8a78c04d52d32532297c2ef3"
+                    >0x9adc7710e9d1b29d8a78c04d52d32532297c2ef3</a
+                  >
+                </li>
+                <li class="py-1">Max Total Supply: <b>600 Milions</b></li>
+              </ul>
             </div>
-            <div class="row">
-              <div class="col">
-                <h6 class="violet text-uppercase font-weight-bold">Quadrans Links</h6>
-                <div class="row">
-                  <div class="col d-flex justify-content-start mt-3">
-                    <a href="https://quadrans.io" target="_blank" class="violet mb-auto mx-2">
-                      <div class="card-link d-flex text-center" style="width: 125px">
-                        <i class="fas fa-globe violet mt-auto" style="font-size: 32px"></i>
-                        <small>Website</small>
-                      </div>
-                    </a>
-                    <a href="https://t.me/quadrans" target="_blank" class="violet mb-auto mx-2">
-                      <div class="card-link d-flex text-center" style="width: 125">
-                        <i class="fab fa-telegram violet mt-auto" style="font-size: 32px"></i>
-                        <small>Community</small>
-                      </div>
-                    </a>
-                  </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <h6 class="violet text-uppercase font-weight-bold">
+                Quadrans Links
+              </h6>
+              <div class="row">
+                <div class="col d-flex justify-content-start mt-3">
+                  <a
+                    href="https://quadrans.io"
+                    target="_blank"
+                    class="violet mb-auto mx-2"
+                  >
+                    <div
+                      class="card-link d-flex text-center"
+                      style="width: 125px"
+                    >
+                      <i
+                        class="fas fa-globe violet mt-auto"
+                        style="font-size: 32px"
+                      ></i>
+                      <small>Website</small>
+                    </div>
+                  </a>
+                  <a
+                    href="https://t.me/quadrans"
+                    target="_blank"
+                    class="violet mb-auto mx-2"
+                  >
+                    <div
+                      class="card-link d-flex text-center"
+                      style="width: 125"
+                    >
+                      <i
+                        class="fab fa-telegram violet mt-auto"
+                        style="font-size: 32px"
+                      ></i>
+                      <small>Community</small>
+                    </div>
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-</div>
+    </div>
+  </div>
 </template>
